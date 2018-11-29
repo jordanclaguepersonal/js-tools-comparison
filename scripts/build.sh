db8a6a27555b08ac072cdb3b2decfae8a188c487
Jordan Clague <jordaneuanclague+code@gmail.com>
2018-11-29 23:02:26 +0000

    Building Vue.js HW app and updating link

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,4 +6,9 @@ echo "Building React app located at: $reactDirectory"
 cd "$reactDirectory" && npm i
 cd "$reactDirectory" && echo \"Bundling...\" && npx parcel build index.html -d ../dist/react --public-url ./
 
+vuejsDirectory="$startingDirectory/vuejs"
+echo "Building Vue.js app located at: $vuejsDirectory"
+cd "$vuejsDirectory" && npm i
+cd "$vuejsDirectory" && echo \"Bundling...\" && npx parcel build index.html -d ../dist/vuejs --public-url ./
+
 exit 0
