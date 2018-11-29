fa7c5e0487c77cd1dbc0915671ae89e18c404470
Jordan Clague <jordaneuanclague+code@gmail.com>
2018-11-29 22:35:37 +0000

    Making bash script responsible for top-level dist/ with each app only able to build to it's own directory

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,7 @@ startingDirectory="$PWD"
 
 reactDirectory="$startingDirectory/react"
 echo "Building React app located at: $reactDirectory"
-cd "$reactDirectory" && npm i && npm run build
+cd "$reactDirectory" && npm i
+cd "$reactDirectory" && echo \"Bundling...\" && npx parcel build index.html -d ../dist/react --public-url ./
 
 exit 0
