8e4a35534c93a70af3a683eedb1d458c85df31fe
Jordan Clague <jordaneuanclague+code@gmail.com>
2020-11-05 16:31:33 +0000

    Updating build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,4 +11,9 @@ echo "Building Vue.js app located at: $vuejsDirectory"
 cd "$vuejsDirectory" && npm i
 cd "$vuejsDirectory" && echo \"Bundling...\" && npx parcel build index.html -d ../dist/vuejs --public-url ./
 
+angularjsDirectory="$startingDirectory/angular"
+echo "Building Angular.js app located at: $angularjsDirectory"
+cd "$angularjsDirectory" && npm i
+cd "$angularjsDirectory" && echo \"Bundling...\" && npx ng build --output-path ../dist/angular --baseHref ./
+
 exit 0
